9c90bdf036a3757e799ead1466573456d2d2788d
irenva101 <80257279+irenva101@users.noreply.github.com>
2022-06-07 14:20:47 +0200

    Update load_balancer.py

--- RES_Projekat/load_balancer.py
+++ RES_Projekat/load_balancer.py
@@ -1,6 +1,5 @@
 
-class LoadBalancer():
-    
-    @staticmethod
+class LoadBalancer(): 
+@staticmethod
 def LB_Reciever(item):
     dataset=[item.code, item.value] #data set mi je niz od dve stvari
